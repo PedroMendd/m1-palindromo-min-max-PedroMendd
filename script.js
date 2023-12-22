828f2c079d6b2c40ce05b8f0bba25fa32a4307dd
Pedro Mendonça <althander@gmail.com>
2023-12-22 16:21:08 -0300

    Atividade 1 e 2
    Palindromo atividade 1 e 2

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    str = str.replace(/\s/g, '').toUpperCase()
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != str[str.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
 }
+  
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let min = arr[0]
+    let max = arr[0]
+
+    for (let i = 0; i <= arr.length; i++){
+        if (arr[i] < min){
+            min = arr[i]
+        } else if (arr[i] > max){
+            max = arr[i]
+        }
+    }
+
+    return [min, max]
 }
