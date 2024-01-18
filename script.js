9b33715532d0b8cd3d0483b031d539c9d614bdf8
Pedro Mendonça <althander@gmail.com>
2024-01-18 16:02:51 -0300

    Trocando o regex
    atualização após revisão

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function isPalindrome(str){
-    str = str.replace(/\s/g, '').toUpperCase()
+    str = str.replaceAll(' ', '').toUpperCase()
 
     for (let i = 0; i < str.length; i++) {
         if (str[i] != str[str.length - 1 - i]) {
